76d5bc7ed73c2ff0c9ad35b86fc25293be9077ab
LiDong <ldwqh0@outlook.com>
2021-07-17 15:17:24 +0800

    添加日志记录

--- src/appendErr.mjs
+++ src/appendErr.mjs
@@ -1,4 +1,4 @@
-import { writeFile } from 'fs/promiese'
+import { writeFile } from 'fs/promises'
 
 export default function (url) {
   return writeFile(writeFile(new URL(`../out/err.log`, import.meta.url), url, { encoding: 'utf8', mode: 'a' }))
