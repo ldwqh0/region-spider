0c3c6836176997f46cc83ae7f59a777030d4511b
LiDong <ldwqh0@outlook.com>
2021-07-17 15:15:06 +0800

    添加日志记录

--- src/readFromUrl.mjs
+++ src/readFromUrl.mjs
@@ -21,7 +21,7 @@ export default async function (url, encoding) {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.67'
       }
     })
-    // console.log(`读取文件${url}完成`)
+    console.log(`读取文件${url}完成`)
     return iconv.decode(Buffer.from(data), encoding)
   } catch (e) {
     await appendErr(url)
