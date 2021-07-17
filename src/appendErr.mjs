1d4fa80132b7e60d64bda5d4dd0ffbd0b2086873
LiDong <ldwqh0@outlook.com>
2021-07-17 17:10:23 +0800

    修改错误追加逻辑

--- src/appendErr.mjs
+++ src/appendErr.mjs
@@ -1,5 +1,5 @@
 import { writeFile } from 'fs/promises'
 
 export default function (url) {
-  return writeFile(writeFile(new URL(`../out/err.log`, import.meta.url), url, { encoding: 'utf8', mode: 'a' }))
+  return writeFile(new URL(`../out/err.log`, import.meta.url), `${url}\r\n`, { encoding: 'utf8', flag: 'a' })
 }
--- src/appendVillages.mjs
+++ src/appendVillages.mjs
@@ -1,5 +1,5 @@
 import { writeFile } from 'fs/promises'
 
 export default async function (villiages) {
-  return await writeFile(new URL(`../out/villages.abort.json`, import.meta.url), JSON.stringify(data), 'utf8')
+  return await writeFile(new URL(`../out/villages.abort.json`, import.meta.url), JSON.stringify(villiages), 'utf8')
 }
